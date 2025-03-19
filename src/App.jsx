dab2639ffc405020d42945a070b9cb25d33aa672
festus-sulumeti <sulumetifestus@outlook.com>
2025-03-19 21:43:01 +0300

    Feat: added the sigup route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
-import Login from './components/pages/Login'; // Import the Login component
+import Login from './components/pages/Login';
+import Signup from './components/pages/Signup';
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} /> {/* Add the login route */}
+        <Route path="/login" element={<Login />} /> 
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );

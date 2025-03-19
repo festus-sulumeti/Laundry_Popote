5a38c17bdda10f29e972f99ed5e450c6b1f31313
festus-sulumeti <sulumetifestus@outlook.com>
2025-03-19 21:28:29 +0300

    feat: add routing with react-router-dom and login route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import Login from './components/pages/Login'; // Import the Login component
 
 export default function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} /> {/* Add the login route */}
+      </Routes>
+    </Router>
   );
 }

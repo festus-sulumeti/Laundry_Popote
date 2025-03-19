bf1ea5e7bb3b48828521e144877891218ded906c
festus-sulumeti <sulumetifestus@outlook.com>
2025-03-19 21:40:38 +0300

    feat: add navigation to signup page on button click

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -35,7 +35,9 @@ const LaundryLanding = () => {
                         Sign In
                     </button>
                     <br />
-                    <button className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600">SignUp</button>
+                    <button className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600"
+                     onClick={() => navigate('/signup')}
+                    >SignUp</button>
                 </div>
                
             </nav>

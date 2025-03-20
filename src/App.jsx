23e1497e9c2b10b939d1707b5b34f46461282e5e
festus-sulumeti <sulumetifestus@outlook.com>
2025-03-20 22:05:54 +0300

    feat: added the forgot password route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/pages/Login';
 import Signup from './components/pages/Signup';
+import ForgotPassword from './components/pages/Forgotpaasword';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} /> 
         <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </Router>
   );
